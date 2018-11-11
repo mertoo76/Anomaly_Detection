46f502da2cd6890bd27d029a3e1af200ea8455f5
mertoo76 <mertgun76@gmail.com>
2018-11-11 16:55:31 +0300

    justANN-EMDANN

---
+++ original-data-with-ann/ANNWithoutEMD.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  8 14:33:16 2018
+
+@author: user
+"""
+
+from PyEMD import EMD
+import numpy as np
+import pylab as plt
+
+from sklearn import datasets
+from sklearn.preprocessing import LabelEncoder
+
+
+#Load dataset
+kdd=datasets.fetch_kddcup99(percent10=True)
+
+#Prepare input signal for EMD
+x=kdd.data[:,4:6]
+x=np.append(x, kdd.data[:,22:24], axis=1)
+x=np.append(x, kdd.data[:,31:33], axis=1)
+
+#labels
+y=kdd.target.copy()
+normal=y[0]
+for i in range(len(y)):
+    if y[i] != normal:
+        #print(y_prep[i])
+        y[i]='attack'
+    else:
+        y[i]='normal'
+#1 normal, 0 attack
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
+
+#splitting the dataset into the training set and test set
+from sklearn.cross_validation import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.3, random_state = 0)
+
+
+
+# Importing the Keras libraries and packages
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+# Initialising the ANN
+classifier = Sequential()
+
+# Adding the input layer and the first hidden layer
+classifier.add(Dense(output_dim = 60, init = 'uniform', activation = 'relu', input_dim = 6))
+
+#Adding a second hidden layer
+classifier.add(Dense(output_dim = 60, init = 'uniform', activation = 'relu'))
+
+#Adding a third hidden layer
+classifier.add(Dense(output_dim = 60, init = 'uniform', activation = 'relu'))
+
+# Adding the output layer
+classifier.add(Dense(output_dim = 1, init = 'uniform', activation = 'sigmoid'))
+
+# Compiling the ANN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Fitting the ANN to the Training set
+classifier.fit(x_train, y_train, batch_size = 10, nb_epoch = 20)
+
+# Predicting the Test set results
+y_pred = classifier.predict(x_test)
+y_pred = (y_pred > 0.5)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+#the performance of the classification model
+print("the Accuracy is: "+ str((cm[0,0]+cm[1,1])/(cm[0,0]+cm[0,1]+cm[1,0]+cm[1,1])))
+recall = cm[1,1]/(cm[0,1]+cm[1,1])
+print("Recall is : "+ str(recall))
+print("False Positive rate: "+ str(cm[1,0]/(cm[0,0]+cm[1,0])))
+precision = cm[1,1]/(cm[1,0]+cm[1,1])
+print("Precision is: "+ str(precision))
+print("F-measure is: "+ str(2*((precision*recall)/(precision+recall))))
+from math import log
+print("Entropy is: "+ str(-precision*log(precision)))
